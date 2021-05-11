1de88e87e755c2a6537970670438aad620d738b0
이태양 책임/전자EA팀 <leesun@lgcns.com>
2021-05-11 17:10:30 +0900

    recursion codes

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,5 +1,18 @@
 public class Recursion {
     public static void main(String[] args) {
+        int n = 2;
 
+        System.out.printf("\nfactorial(" + n + ") = " + factorial(n));
+
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            System.out.printf(n + "");
+            return 1;
+        } else {
+            System.out.printf(n + " x ");
+            return n * factorial(n - 1);
+        }
     }
 }
